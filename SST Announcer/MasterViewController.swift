9bd4dfb6c624a3d176b6f4f7a3abe354496ab37f
Pan Ziyue <hacker@statixind.net>
2015-06-03 22:48:00 +0800

    added a ton of boilerplate code

--- SST Announcer/MasterViewController.swift
+++ SST Announcer/MasterViewController.swift
@@ -8,30 +8,29 @@
 
 import UIKit
 
-class MasterViewController: UITableViewController {
+class MasterViewController: UITableViewController, NSXMLParserDelegate, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate {
     
     // Variables Declaration
-    private let parser : NSXMLParser
-    
-    private var feeds : [AnyObject]
-    private var item : [String:String]
-    private var postTitle : String = ""
-    private var postLink : String = ""
-    private var postDate : String = ""
-    private var postAuthor : String = ""
-    private var postDescription : String = ""
-    
-    private var searchResults : [String]
-    
+    private var parser : NSXMLParser
+    private struct Item {
+        let title : String
+        let link : String
+        let date : String
+        let author : String
+        let description :String
+    }
+    private var feeds : [Item]
+    private var element : String = ""
+    private var searchResults : [Item]
     private let dateFormatter : NSDateFormatter
     
     required init!(coder aDecoder: NSCoder!) {
         // Variables initialization
         parser = NSXMLParser()
         
-        feeds = [AnyObject]()
-        item = Dictionary<String, String>()
-        searchResults = [String]()
+        feeds = [Item]()
+        //item = Dictionary<String, String>()
+        searchResults = [Item]()
         
         dateFormatter = NSDateFormatter()
         dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
@@ -43,17 +42,34 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Init refresh controls
+        let refreshControl : UIRefreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refresh", forControlEvents: UIControlEvents.ValueChanged)
+        self.refreshControl = refreshControl
     }
     
     override func viewWillAppear(animated: Bool) {
+        // Add observer for push to catch push notification messages
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "pushNotificationsReceived", name: "pushReceived", object: nil)
         
+        // Feed parsing contained inside a dispatch_once
         var token : dispatch_once_t = 0
         dispatch_once(&token, {
             MRProgressOverlayView.showOverlayAddedTo(self.tabBarController?.view, title: "Loading...", mode: .IndeterminateSmall, animated: true)
             UIApplication.sharedApplication().networkActivityIndicatorVisible = true
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-                
+                self.feeds = [Item]() //Sort of like alloc init
+                let url = NSURL(string: "http://feeds.feedburner.com/SSTBlog")
+                self.parser = NSXMLParser(contentsOfURL: url)!
+                self.parser.delegate = self
+                self.parser.shouldResolveExternalEntities = false
+                let success = self.parser.parse()
+                if !success {
+                    dispatch_sync(dispatch_get_main_queue(), {
+                        MRProgressOverlayView.showOverlayAddedTo(self.tabBarController?.view, title: "Error Loading!", mode: .Cross, animated: true)
+                        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                    })
+                }
             })
         })
     }
@@ -61,6 +77,45 @@ class MasterViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Search functionality
+    func filterContentForSearchText(searchText: String) {
+        self.searchResults = self.feeds.filter({(post: Item) -> Bool in
+            let stringMatch = post.title.rangeOfString(searchText)
+            return stringMatch != nil
+        })
+    }
+    
+    func shouldReloadTableForSearchString(controller: UISearchDisplayController, searchString: String) -> (Bool) {
+        self.filterContentForSearchText(searchString)
+        return true
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchScope searchOption: Int) -> Bool {
+        self.filterContentForSearchText(self.searchDisplayController!.searchBar.text)
+        return true
+    }
+    
+    // MARK: - NSXMLParser delegate
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]) {
+        
+    }
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String?) {
+        
+    }
+    
+    func parserDidEndDocument(parser: NSXMLParser) {
+        
+    }
+    
+    func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+        
     }
 
     // MARK: - Table view data source
@@ -77,7 +132,6 @@ class MasterViewController: UITableViewController {
         return 0
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
 
@@ -85,44 +139,7 @@ class MasterViewController: UITableViewController {
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -130,6 +147,5 @@ class MasterViewController: UITableViewController {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
