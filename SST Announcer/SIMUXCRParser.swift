060f13c179cc9cf2c4ea4bfb3ef752c4cf3e1ea2
Pan Ziyue <random.rrr3@gmail.com>
2015-07-25 14:09:43 +0800

    currently the leanest, meanest version of the SIMUXCR parser, using JSON data DIRECT from Full-Text RSS, eliminating a lot of unnecessary stuff like decoding HTML entities

--- SST Announcer/SIMUXCRParser.swift
+++ SST Announcer/SIMUXCRParser.swift
@@ -34,9 +34,7 @@ public class SIMUXCRParser: NSObject {
         var returnTuple = (title: "", description: "") // Init empty named tuple
 
         // Test for connectivity to node1.sstinc.org and if not available, fallback to another server
-        let determinateTuple = chooseServer()
-        let urlFirstSegment = determinateTuple.urlPrepend
-        self.errorBoolean = determinateTuple.error
+        let urlFirstSegment = "http://node1.sstinc.org/api/fulltextrss/extract.php?url="
 
         // Actual REST JSON loading, with extremely strict optional error checking
         let url = NSURL(string: "\(urlFirstSegment)\(htmlString)&format=json")
@@ -46,7 +44,7 @@ public class SIMUXCRParser: NSObject {
                     var jsonError: NSError? = nil
                     if let jsonObject = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as? NSDictionary {
                         if jsonError == nil {
-                            if let items = jsonObject["item"] as? NSDictionary, title = items["title"] as? String, description = items["description"] as? String {
+                            if let title = jsonObject["title"] as? String, description = jsonObject["content"] as? String {
                                 returnTuple.title = title
                                 returnTuple.description = description
                             } else {self.errorBoolean = true}
@@ -59,8 +57,6 @@ public class SIMUXCRParser: NSObject {
             if self.errorBoolean {
                 returnTuple.title = "Error"
                 returnTuple.description = "<p align=\"center\">There was a problem loading this article, please check your connection, or try opening the URL in Safari via the share button above.</p>"
-            } else {
-                returnTuple.description = returnTuple.description.stringByDecodingHTMLEntities
             }
 
             completionClosure(title: returnTuple.title, description: returnTuple.description)
@@ -69,115 +65,4 @@ public class SIMUXCRParser: NSObject {
         getData.resume()
     }
 
-    // MARK: - Private functions
-
-    private func chooseServer() -> (urlPrepend: String, error: Bool) {
-        let testUrl = NSURL(string: "http://node1.sstinc.org/api/check.json")
-        var errorPgm = false
-        var useFallback = false
-        let test = NSURLSession.sharedSession().dataTaskWithURL(testUrl!) {(data, response, error) in
-            if error == nil {
-                if let rsp = response as? NSHTTPURLResponse {
-                    if rsp.statusCode != 200 { // Use fallback here
-                        useFallback = true
-                    } else {
-                        useFallback = false
-                    }
-                } else {
-                    errorPgm = true
-                }
-            } else {
-                useFallback = true
-            }
-        }
-
-        if useFallback {
-            return ("https://api.statixind.net/v1/clear?url=", errorPgm)
-        } else {
-            return ("http://node1.sstinc.org/api/v1/clear?url=", errorPgm)
-        }
-    }
-}
-
-// MARK: - Extensions
-
-extension String {
-
-    /// Returns a new string made by replacing in the `String`
-    /// all HTML character entity references with the corresponding
-    /// character.
-
-    var stringByDecodingHTMLEntities: String {
-
-        let characterEntities : [String:Character] = [
-            // XML predefined entities:
-            "&quot;"    : "\"",
-            "&amp;"     : "&",
-            "&apos;"    : "'",
-            "&lt;"      : "<",
-            "&gt;"      : ">",
-
-            // HTML character entity references:
-            "&nbsp;"    : "\u{00a0}",
-            // ...
-            "&diams;"   : "♦",
-        ]
-
-        // ===== Utility functions =====
-        // Convert the number in the string to the corresponding
-        // Unicode character, e.g.
-        //    decodeNumeric("64", 10)   --> "@"
-        //    decodeNumeric("20ac", 16) --> "€"
-        func decodeNumeric(string: String, base: Int32) -> Character? {
-            let code = UInt32(strtoul(string, nil, base))
-            return Character(UnicodeScalar(code))
-        }
-
-        // Decode the HTML character entity to the corresponding
-        // Unicode character, return `nil` for invalid input.
-        //     decode("&#64;")    --> "@"
-        //     decode("&#x20ac;") --> "€"
-        //     decode("&lt;")     --> "<"
-        //     decode("&foo;")    --> nil
-        func decode(entity: String) -> Character? {
-            if entity.hasPrefix("&#x") || entity.hasPrefix("&#X"){
-                return decodeNumeric(entity.substringFromIndex(advance(entity.startIndex, 3)), 16)
-            } else if entity.hasPrefix("&#") {
-                return decodeNumeric(entity.substringFromIndex(advance(entity.startIndex, 2)), 10)
-            } else {
-                return characterEntities[entity]
-            }
-        }
-
-        // ===== Method starts here =====
-
-        var result = ""
-        var position = startIndex
-
-        // Find the next '&' and copy the characters preceding it to `result`:
-        while let ampRange = self.rangeOfString("&", range: position ..< endIndex) {
-            result.extend(self[position ..< ampRange.startIndex])
-            position = ampRange.startIndex
-
-            // Find the next ';' and copy everything from '&' to ';' into `entity`
-            if let semiRange = self.rangeOfString(";", range: position ..< endIndex) {
-                let entity = self[position ..< semiRange.endIndex]
-                position = semiRange.endIndex
-
-                if let decoded = decode(entity) {
-                    // Replace by decoded character:
-                    result.append(decoded)
-                } else {
-                    // Invalid entity, copy verbatim:
-                    result.extend(entity)
-                }
-            } else {
-                // No matching ';'.
-                break
-            }
-        }
-        // Copy remaining characters to `result`:
-        result.extend(self[position ..< endIndex])
-        return result
-    }
 }
